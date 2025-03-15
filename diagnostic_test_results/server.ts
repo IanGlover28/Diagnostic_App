b8693c420c35ce7888fb9a875aae36f4f6d360a6
Ian Titus Glover <iantitusglover2@gmail.com>
2025-03-15 13:55:42 +0000

    relocating index.tsx

--- diagnostic_test_results/server.ts
+++ diagnostic_test_results/server.ts
@@ -74,6 +74,7 @@ app.get(
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
   res.status(500).json({ message: "Internal server error" });
+  next();
 });
 
 // Start the server
